898758b946c3084490d4a87c7796f48c1d5a482f
Pierre Mevel <pierre.mevel@student.ecp.fr>
2018-10-28 17:28:26 +0100

    test prettier bis

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from "./app";
 
 const server = app.listen(app.get("port"), () => {
+  // tslint:disable-next-line
   console.log("Application is running on port %s, in mode %s", app.get("port", app.get("env")))
 });
+export default server;
