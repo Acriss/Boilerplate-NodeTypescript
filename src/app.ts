cd2caf363601ac8c807b62721b6da8318e5ea998
Pierre Mevel <pierre.mevel@student.ecp.fr>
2018-10-28 18:27:14 +0100

    more config

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+
+// Loading environment variables into process.env
+if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: ".env" });
+} else if (process.env.NODE_ENV === "development") {
+  dotenv.config({ path: ".env.development" });
+}
 
 const app = express();
 
